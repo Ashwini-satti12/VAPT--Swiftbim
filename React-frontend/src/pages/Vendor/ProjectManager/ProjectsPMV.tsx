bf123a25b94b428f6f379639a047283e8ac987cc
Ashwini-satti12 <ashwini.mine@gmail.com>
2026-04-08 20:13:35 +0530

    pulling code

--- React-frontend/src/pages/Vendor/ProjectManager/ProjectsPMV.tsx
+++ React-frontend/src/pages/Vendor/ProjectManager/ProjectsPMV.tsx
@@ -516,7 +516,6 @@ export default function ProjectsPMV() {
             project_manager_id: nameToId(createProjectManager, projectManagers),
             start_date: createStartDate,
             due_date: createEndDate,
-            due_date: createEndDate,
             totalhours: createTotalHours,
             perday: createPerDay,
             lead_id: nameToId(createBIMLead, bimLeads),
